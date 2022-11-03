f0cb00a25a97c35333ce266d28c176ac3ba165e2
amycoughlin-acg <72401415+amycoughlin-acg@users.noreply.github.com>
2022-11-03 17:08:23 -0500

    Update ProgramSolution.cs

--- DP420/ConflictResolution/ProgramSolution.cs
+++ DP420/ConflictResolution/ProgramSolution.cs
@@ -9,7 +9,7 @@ class ProgramToo
     {
     static async Task MainToo(string[] args)
         {   
-            string myConnectionString = "[ConnectionString]";
+             string myConnectionString = "[ConnectionString]";
 
             //Declare a CosmosClient using the connection string
             CosmosClient aClient = new (myConnectionString);
@@ -19,18 +19,16 @@ class ProgramToo
             //Get a pre-deployed database from the Cosmos DB account called "LabDB";
             Database myDatabase = aClient.GetDatabase("LabDB");
 
-            //Declare a ContainerProperties object with the minimum properties:
+            //Declare a ContainerProperties object called containerProps, with the minimum properties:
             //  "LabItems" for the container name and "/labPK" for the partition key path;
             ContainerProperties containerProps = new ContainerProperties("LabItems","/labPK");
 
             //Add a ConflictResolutionPolicy to containerProps with the two properties appropriate to the lab scenario;
-            containerProps.ConflictResolutionPolicy.Mode = Mode.LastWriterWins;
+            containerProps.ConflictResolutionPolicy.Mode = ConflictResolutionMode.LastWriterWins;
             containerProps.ConflictResolutionPolicy.ResolutionPath = "/myAltTimeStamp";
 
             //Create the new container, taking containerProps as a single parameter
             ContainerResponse response = await myDatabase.CreateContainerIfNotExistsAsync(containerProps);
-
-//A less verbose and, arguably, more elegant way to accomplish the same thing:
 
         // ContainerResponse response = await aClient.GetDatabase("LabDB")
         //     .CreateContainerIfNotExistsAsync(new ContainerProperties("LabItems", "/labPK")
