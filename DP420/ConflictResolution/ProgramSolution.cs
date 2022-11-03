c15e61668763deee0b07c416e7acaaa43a824a2a
amycoughlin-acg <72401415+amycoughlin-acg@users.noreply.github.com>
2022-11-03 14:05:52 -0500

    Update ProgramSolution.cs

--- DP420/ConflictResolution/ProgramSolution.cs
+++ DP420/ConflictResolution/ProgramSolution.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
-using Bogus;
 
 namespace ConflictResolution
 {
@@ -9,22 +8,41 @@ namespace ConflictResolution
 class Program
     {
     static async Task Main(string[] args)
-        {   string myConnectionString =
-            "https://dp420.documents.azure.com:443/;notARealKeyfDR2ci9QgkdkvERTQ==";
+        {   
+            string myConnectionString = "[ConnectionString]";
 
-CosmosClient aClient = new (myConnectionString);
+            //Declare a CosmosClient using the connection string
+            CosmosClient aClient = new (myConnectionString);
 
-Container container = await aClient.GetDatabase("DatabaseForLab")
-    .CreateContainerIfNotExistsAsync(new ContainerProperties("LabItems", "/labPK")
-     {
-        ConflictResolutionPolicy = new ConflictResolutionPolicy()
-        {
-            Mode = ConflictResolutionMode.LastWriterWins,
-            ResolutionPath = "/myAltTimeStamp"
+//A slightly more verbose approach but that is somewhat easier to build in a step-wise manner
 
-        }
-    }
-    );
+            //Get a pre-deployed database from the Cosmos DB account called "LabDB";
+            Database myDatabase = aClient.GetDatabase("LabDB");
+
+            //Declare a ContainerProperties object with the minimum properties:
+            //  "LabItems" for the container name and "/labPK" for the partition key path;
+            ContainerProperties containerProps = new ContainerProperties("LabItems","/labPK");
+
+            //Add a ConflictResolutionPolicy to containerProps with the two properties appropriate to the lab scenario;
+            containerProps.ConflictResolutionPolicy.Mode = Mode.LastWriterWins;
+            containerProps.ConflictResolutionPolicy.ResolutionPath = "/myAltTimeStamp";
+
+            //Create the new container, taking containerProps as a single parameter
+            Container container = await myDatabase.CreateContainerIfNotExistsAsync(containerProps);
+
+//A less verbose and, arguably, more elegant way to accomplish the same thing:
+
+        // Container container = await aClient.GetDatabase("LabDB")
+        //     .CreateContainerIfNotExistsAsync(new ContainerProperties("LabItems", "/labPK")
+        //      {
+        //         ConflictResolutionPolicy = new ConflictResolutionPolicy()
+        //         {
+        //             Mode = ConflictResolutionMode.LastWriterWins,
+        //             ResolutionPath = "/myAltTimeStamp"
+
+        //         }
+        //     }
+        //     );
 
     }
   }
