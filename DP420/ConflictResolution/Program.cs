12f4da203c84a60e1976d7325f3b8e7f5b3e92e8
amycoughlin-acg <72401415+amycoughlin-acg@users.noreply.github.com>
2022-11-03 14:03:55 -0500

    Update Program.cs

--- DP420/ConflictResolution/Program.cs
+++ DP420/ConflictResolution/Program.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
-using Bogus;
 
 namespace ConflictResolution
 {
@@ -10,9 +9,27 @@ class Program
     {
     static async Task Main(string[] args)
         {   
+            //Copy the primary connection string from the Cosmos DB account in the portal and
+            string myConnectionString = "[ConnectionString]";
+
+            //Declare a CosmosClient using the connection string
+
+
+            //Get a pre-deployed database from the Cosmos DB account called "LabDB";
+
+
+
+            //Declare a ContainerProperties object with the minimum properties:
+                //"LabItems" for the container name and "/labPK" for the partition key path;
+
+
+
+            //Add a ConflictResolutionPolicy to containerProps with the two properties appropriate to the lab scenario;
+
+
+            //Create the new container, taking containerProps as a single parameter
 
 
         }
-  
     }
 }
